0b6bf56df0e99b054f25f762025027b9ed1e3f67
ureyni <hasan.ucak@gmail.com>
2015-11-09 12:54:16 +0200

    master

--- git.sh
+++
@@ -1,6 +0,0 @@
-echo "# postfix.3.1" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/ureyni/postfix.3.1.git
-git push -u origin master
